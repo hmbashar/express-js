a480c24437f9f399b5d4023fd114c69d211bfbea
hmbashar <hmbashar@gmail.com>
2024-10-25 16:29:06 +0600

    response status codes

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.post('/two', function(request, response) {
     response.end("This is simple string response two");
 });
 
+
+app.get('/three', function(request, response) {
+    response.status(201).end("Unauthorized code");
+})
+
 app.listen(8000, function() {
     console.log('Server running on port 8000');
 })
