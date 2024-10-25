6698e35b296af4446199954929dd99695fdafea3
hmbashar <hmbashar@gmail.com>
2024-10-25 15:46:19 +0600

    git init

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+let express = require('express');
+
+app = express();
+
+
+app.get('/', function(request, response) {
+    response.send("Hello Express!");
+});
+
+app.listen(8000, function() {
+    console.log('Server running on port 8080');
+})
