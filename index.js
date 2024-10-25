8a9720b00e9da467dba2302392f29a0583a96cab
hmbashar <hmbashar@gmail.com>
2024-10-25 16:14:05 +0600

    express routing

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ app = express();
 app.get('/', function(request, response) {
     response.send("Hello Express!");
 });
+app.post('/about', function(request, response) {
+    response.send("Hello Express! From About Page");
+});
+app.put('/contact', function(request, response) {
+    response.send("Hello Express! from Contact Page");
+});
+app.delete('/team', function(request, response) {
+    response.send("Hello Express! From Team");
+});
 
 app.listen(8000, function() {
-    console.log('Server running on port 8080');
+    console.log('Server running on port 8000');
 })
