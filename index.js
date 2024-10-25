c84fe8258f3eee37644be1514e965154e25bcbe3
hmbashar <hmbashar@gmail.com>
2024-10-25 16:25:42 +0600

    simple string response

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ let express = require('express');
 app = express();
 
 
-app.get('/', function(request, response) {
-    response.send("Hello Express!");
+// app.get('/one', function(request, response) {
+//     response.send("This is simple string response");
+// });
+//
+// app.post('/two', function(request, response) {
+//     response.send("This is simple string response two");
+// });
+
+app.get('/one', function(request, response) {
+    response.end("This is simple string response");
 });
-app.post('/about', function(request, response) {
-    response.send("Hello Express! From About Page");
-});
-app.put('/contact', function(request, response) {
-    response.send("Hello Express! from Contact Page");
-});
-app.delete('/team', function(request, response) {
-    response.send("Hello Express! From Team");
+
+app.post('/two', function(request, response) {
+    response.end("This is simple string response two");
 });
 
 app.listen(8000, function() {
