defe5a538fc6ad3841e09ffc269b2aa352c14fc0
hmbashar <hmbashar@gmail.com>
2024-10-25 16:35:24 +0600

    JSON Response

--- index.js
+++ index.js
@@ -19,10 +19,32 @@ app.post('/two', function(request, response) {
     response.end("This is simple string response two");
 });
 
-
+//Response Status Code
 app.get('/three', function(request, response) {
     response.status(201).end("Unauthorized code");
-})
+});
+
+//JSON Response
+app.get('/four', function(request, response) {
+   let MyJsonArry = [
+       {
+           Name: "John Doe",
+           City: "Dhaka",
+           Occupation: "Developer"
+       },
+       {
+           Name: "Bashar",
+           City: "Patuakhali",
+           Occupation: "Developer"
+       },
+       {
+           Name: "Shamim",
+           City: "Patuakhali",
+           Occupation: "Student"
+       }
+   ];
+    response.json(MyJsonArry);
+});
 
 app.listen(8000, function() {
     console.log('Server running on port 8000');
